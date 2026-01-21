d7e60fbda9d5065518d18a36d8bf65e00c3c4640
DiyanAmin <pakuptl3112@gamil.com>
2026-01-21 19:09:20 +0530

    done

---
+++ Module 5/Lesson 1/Afterclass.js
@@ -0,0 +1,7 @@
+var num1 =Math.floor(Math.random()*99999)+1;
+var num2 =Math.floor(Math.random()*9999)+1;
+var num3 =Math.floor(Math.random()*999)+1;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
