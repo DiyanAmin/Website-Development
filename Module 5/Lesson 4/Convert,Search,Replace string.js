b08b96bda09ce9f766f91e91962f47f5a71a720a
DiyanAmin <pakuptl3112@gamil.com>
2026-01-28 19:00:00 +0530

    done

---
+++ Module 5/Lesson 4/Convert,Search,Replace string.js
@@ -0,0 +1,15 @@
+var bigNumber = 9999999;
+var smallNumber = 11;
+
+//Conacation
+var anotherNumber = String(bigNumber) + String(smallNumber);
+console.log(anotherNumber);
+
+try{
+    // Welcome('bob');
+    console.log('hi')
+}
+catch(err){
+    console.log("Error")
+}
+
